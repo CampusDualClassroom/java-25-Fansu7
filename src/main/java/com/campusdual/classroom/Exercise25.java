fb94d35da17372acfe063e9a1778e88614496ab9
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2025-01-30 08:51:26 +0100

    Refactorizar metodos printAndEmptyStack y createStack

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -5,17 +5,20 @@ import java.util.Stack;
 public class Exercise25 {
     public static Stack<String> createStack() {
         Stack<String> myStack = new Stack<>();
-        String[] nombresArray =  {"Smith","Montessori","Peralta","House"};
-        for(String s: nombresArray){
-            myStack.push(s);
-        }
+//        String[] nombresArray =  {"Smith","Montessori","Peralta","House"};
+//        for(String s: nombresArray){
+//            myStack.push(s);
+//        }
+        myStack.push("Smith");
+        myStack.push("Montessori");
+        myStack.push("Peralta");
+        myStack.push("House");
         return myStack;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
         while (!stack.isEmpty()){
-            System.out.println(stack.peek());
-            stack.pop();
+            System.out.println(stack.pop());
         }
     }
 
